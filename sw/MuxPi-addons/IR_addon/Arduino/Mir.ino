72f06504c111905229ff205ff7ff2d41a91bea11
k.gorska <k.gorska@samsung.com>
2018-06-06 11:04:38 +0200

    Send IR codes read from serial
    This is main arduino code.
    
    Program uses Arduino IRRemote library[1]
    
    It reads raw codes sent by serial, converts them and emitts proper IR
    signal.
    
    Code should be represented as a string of numbers delimited with commas.
    
    Example:
    key_power_code="4529,4482,575,1665,567,1666,569,1670,575,569,545,546,567,546,567,575,545,575,545,1692,546,1666,567,1696,545,569,546,545,567,567,545,567,545,575,545,542,577,1692,545,567,545,546,567,575,542,575,545,567,545,545,573,1692,542,545,567,1696,545,1689,545,1666,569,1670,575,1665,573,1665,569"
    
    More codes will be commited soon.
    
    All raw codes were read using RedRat and used library is able to send
    proper signals.
    
    [1] https://github.com/z3t0/Arduino-IRremote
    
    Change-Id: Ia3b7512c89a415cc3b6cc273643975dc2db12e4b
    Signed-off-by: k.gorska <k.gorska@samsung.com>

---
+++ sw/MuxPi-addons/IR_addon/Arduino/Mir.ino
@@ -0,0 +1,81 @@
+/*
+    Copyright (c) 2018 Samsung Electronics Co., Ltd All Rights Reserved
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License
+*/
+
+// MIR : MuxPi IR Addon
+// This code is responsible for MIR's microcontroller work.
+// MIR reads raw code from serial, and sends apropriate IR signal.
+#include <IRremote.h>
+
+IRsend irsend;
+
+// Addon LED setup
+const int RED = 13;
+// Samsung TV remote carrier frequency
+const int kHz = 38;
+// Samsung TV remote raw data length
+const size_t bufferSize = 67;
+
+// sendCode executes IR data transmission.
+// During transmission the red led is on to indicate data transfer.
+void sendCode(unsigned int *code)
+{
+  digitalWrite(RED, HIGH);
+  irsend.sendRaw(code, bufferSize, kHz);
+  digitalWrite(RED, LOW);
+}
+
+// processData prepares data and sends apropriate IR signal
+//
+// FIXME: sendSamsung() from IRremote library does not work properly,
+// That is why this workaround is needed.
+void processData(String raw)
+{
+  // For debug purposes
+  // Serial.print(raw);
+  size_t index = 0;
+  // Convert string to unsignet int array
+  char *str = (char *)raw.c_str();
+  unsigned int arr[bufferSize];
+  // Split string
+  char *p = strtok(str, ",");
+
+  // Convert raw data (string) to unsigned int array.
+  while (p != NULL && index < bufferSize)
+  {
+    arr[index++] = (unsigned int)atoi(p);
+    p = strtok(NULL, ",");
+  }
+  // Send raw code
+  sendCode(arr);
+}
+
+void setup()
+{
+  //LEDs setup
+  pinMode(RED, OUTPUT);
+
+  // Serial setup
+  Serial.begin(57600);
+  Serial.flush();
+}
+
+void loop()
+{
+  while (Serial.available() > 0)
+  {
+    processData(Serial.readString());
+  }
+}
